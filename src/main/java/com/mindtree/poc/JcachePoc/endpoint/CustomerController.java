1cac3ee92d59ad30417823ea90771dea012eb41e
Vikas Chaudhary <M1052611@mindtree.com>
2019-08-26 18:48:24 -0400

    tested and implemented Cache-Control

--- src/main/java/com/mindtree/poc/JcachePoc/endpoint/CustomerController.java
+++ src/main/java/com/mindtree/poc/JcachePoc/endpoint/CustomerController.java
@@ -1,7 +1,11 @@
 package com.mindtree.poc.JcachePoc.endpoint;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +22,14 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 	
-	@RequestMapping("/customerDetails")
+	@GetMapping("/customerDetails")
 	//@GET
 	//@Path("customerDetails/{customerId}")
 	//public Customer getCustomerDetails( @PathParam("customerId")int customerId ) {
-	public Customer getCustomerDetails( @RequestParam int customerId ) {
-		Customer customer= customerService.getCustomerDetails(customerId);
+	public ResponseEntity<Customer> getCustomerDetails( @RequestParam("customerId")int customerId) {
+		Customer customer= customerService.getCustomerDetails(105);
 		log.info("Fetching customer details: "+customer );
-		return customer;
-		
+		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(900, TimeUnit.SECONDS)).body(customer);		
 	}
 	
 	/*@GetMapping("/customerDetails")
